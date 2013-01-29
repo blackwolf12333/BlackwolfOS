53eb5c511927cd00a1bed7d645c33d987baf95d3
blackwolf12333 <blackwolf12333@gmail.com>
2013-01-29 14:44:31 +0100

    Update src/arrays.c

--- src/arrays.c
+++ src/arrays.c
@@ -1,5 +1,13 @@
 #include "arrays.h"
 
 int indexOf(char[] array, char c) {
-
+	int i;
+	for(i = 0; i < sizeof(array); i++) {
+		if(array[i] == c) {
+			return i;
+		} else {
+			continue;
+		}
+	}
+	return -1; // not in the array
 }
