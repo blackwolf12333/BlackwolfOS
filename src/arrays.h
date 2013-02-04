c97e3a1335cb0c44a5f10ffe7b8613a2a560fbc6
blackwolf12333 <blackwolf12333@gmail.com>
2013-02-04 15:04:52 +0100

    Update src/arrays.h
    Removed struct, isn't really usefull

--- src/arrays.h
+++ src/arrays.h
@@ -1,12 +1,6 @@
 #ifndef ARRAYS_H
 #define ARRAYS_H
 
-typedef struct array {
-    char array[];
-    int length;
-    int (*indexOf)(char c);
-}
-
 int indexOf(char array[], char c);
 int arrayLength(char array[]);
 
