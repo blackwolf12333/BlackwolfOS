f20d3bd1e6d0affc7f3ebe85a5abd597b57c44a6
blackwolf12333 <blackwolf12333@gmail.com>
2013-02-19 13:54:36 +0100

    Update api/linked_list.c
    Implemented place_after(node*,void*)
    That will place a new node after the node specified as first argument

--- api/linked_list.c
+++ api/linked_list.c
@@ -1,8 +1,19 @@
 #include "linked_list.h"
+#include "stdio.h"
 
 node *start_list(void *data) {
     node n;
     n.data = data;
     n.next = NULL;
     return &n;
+}
+
+void place_after(node *after, void *data) {
+    node *next;
+    node new;
+    new.data = data;
+    new.next = after->next;
+    while((next = new->next) != NULL) {
+        next->next = next;
+    }
 }
